23d253ea69390536400da86385de7f9b41b8b67b
ynm3n <43065779+ynm3n@users.noreply.github.com>
2024-01-12 22:07:09 +0900

    db connection

--- api/index.go
+++ api/index.go
@@ -1,10 +1,23 @@
 package handler
 
 import (
-	"fmt"
+	"context"
+	"database/sql"
 	"net/http"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "hello")
+	dsn := os.Getenv("POSTGRES_URL")
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	ctx := context.Background()
+	if err := db.PingContext(ctx); err != nil {
+		panic(err)
+	}
 }
